78e9f2317646e8a651ef24ae81f18b4e42a4ee66
twishajain <33196561+twishajain@users.noreply.github.com>
2019-01-28 14:20:53 +0530

    Create lab4q1.asm

---
+++ lab4q1.asm
@@ -0,0 +1,22 @@
+Data Segment
+        A dw 0FFFFH
+        B dw 1112H
+        res dd ?
+DATA ENDS
+Code Segment
+assume cs:code,ds:data
+start:
+        mov ax,data
+        mov ds,ax
+        mov ax,A
+        mov bx,B
+        mul BX
+        lea si,res
+        mov word ptr [si],dx
+        inc si
+        inc si
+        mov word ptr [si],ax
+        mov ah,4ch
+        int 21h
+        code ends
+        end  start
